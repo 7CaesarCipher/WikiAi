26929276e08d8278eb356c98b9ca38f915a3f3b1
Shashank Mishra <shashankmshr1510@gmail.com>
2024-01-10 20:59:43 +0530

    Initial Commit

---
+++ Wiki.AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wiki.AI
+//
+//  Created by shashank Mishra on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wiki.AI/Wiki_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wiki_AIApp.swift
+//  Wiki.AI
+//
+//  Created by shashank Mishra on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Wiki_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
